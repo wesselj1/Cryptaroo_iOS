aa82d5f67e40d1aa47e8450cdf4ed35020f057a4
Joey Wessel <joeywessel@Joeys-MacBook-Pro.local>
2012-07-19 20:49:05 -0400

    Next time should probably make a local git to start with

--- QuickCrypt/main.m
+++ QuickCrypt/main.m
@@ -2,17 +2,17 @@
 //  main.m
 //  QuickCrypt
 //
-//  Created by Joey Wessel on 7/19/12.
-//  Copyright (c) 2012 Joey Wessel. All rights reserved.
+//  Created by build on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-#import "AppDelegate.h"
+#import "CAIAppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CAIAppDelegate class]));
     }
 }
