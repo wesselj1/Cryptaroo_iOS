48f8b4f67e98147af3173990fe68012415d6cc0e
Joey Wessel <joeywessel@Joeys-MacBook-Pro.local>
2012-07-31 20:46:44 -0400

    Cleaned up and commented code for iPhone GCDandInverseViewController

--- QuickCrypt/GCDandInverseViewController.h
+++ QuickCrypt/GCDandInverseViewController.h
@@ -10,16 +10,16 @@
 
 @interface GCDandInverseViewController : UIViewController
 {
-    IBOutlet UIView *optionsViewMat;
-    IBOutlet UITextField *inverseOfField;
-    IBOutlet UITextField *modField;
-    IBOutlet UILabel *label1;
+    IBOutlet UIView *optionsViewMat;        // View mat that contains our options
+    IBOutlet UITextField *inverseOfField;   // Field for the integer of which the inverse is desired
+    IBOutlet UITextField *modField;         // Field for the modulus the user would like to use
+    IBOutlet UILabel *label1;               // Labels used for view
     IBOutlet UILabel *label2;
     IBOutlet UILabel *label3;
     IBOutlet UILabel *label4;
-    IBOutlet UITextField *gcd;
-    IBOutlet UITextField *inverse;
-    IBOutlet UIButton *calculate;
+    IBOutlet UITextField *gcd;              // Field for the resulting greatest common denominator of given integer and mod
+    IBOutlet UITextField *inverse;          // Field for the resulting inverse of the given integer in the modulus given
+    IBOutlet UIButton *calculate;           // The calculate button to get inverse and gcd
 }
 
 @property (nonatomic, strong) UIView *optionsViewMat;
@@ -33,8 +33,8 @@
 @property (nonatomic, strong) UITextField *inverse;
 @property (nonatomic, strong) UIButton *caclculate;
 
-- (id)init;
-- (IBAction)dismissKeyboard:(id)sender;
-- (IBAction)calculateButtonPressed:(id)sender;
+- (id)init;                                         // Initializes a GCD and invese view
+- (IBAction)dismissKeyboard:(id)sender;             // Dismisss the keyboard from any of the textFields
+- (IBAction)calculateButtonPressed:(id)sender;      // What to do when the calculate button is pressed
 
 @end
--- QuickCrypt/GCDandInverseViewController.m
+++ QuickCrypt/GCDandInverseViewController.m
@@ -33,6 +33,8 @@
 @synthesize inverse = _inverse;
 @synthesize caclculate = _calculate;
 
+
+#pragma mark - View Lifecycle Methods
 - (id)init
 {
     self = [super initWithNibName:@"GCDandInverseViewController" bundle:nil];
@@ -59,30 +61,38 @@
 	}
 
     
-    td = [TextData textDataManager];
+    td = [TextData textDataManager]; // Get an instance of our textData class
+    
+    // If the options inverseOf and mod exist load them
     if( [td.optionsList objectAtIndex:QCGCDAndInverse] && [[td.optionsList objectAtIndex:QCGCDAndInverse] count] > 0 )
     {
         _inverseOfField.text = [[td.optionsList objectAtIndex:QCGCDAndInverse] objectAtIndex:0];
         _modField.text = [[td.optionsList objectAtIndex:QCGCDAndInverse] objectAtIndex:1];
     }
     
+    // If there are results already for GCDandInverse load them
     if( [[td.outputArray objectAtIndex:QCGCDAndInverse] isKindOfClass:[NSArray class]] && [[td.outputArray objectAtIndex:QCGCDAndInverse] count] > 0 )
     {
         _gcd.text = [[td.outputArray objectAtIndex:QCGCDAndInverse] objectAtIndex:0];
         _inverse.text = [[td.outputArray objectAtIndex:QCGCDAndInverse] objectAtIndex:1];
     }
     else
-    {
+    {   // If no results default to 0
         _gcd.text = @"0";
         _inverse.text = @"0";
     }
     
+    // Create the computer button
     UIImage *blueButtonImage = [[UIImage imageNamed:@"blueButton.png"] stretchableImageWithLeftCapWidth:12 topCapHeight:0];
     [_calculate setBackgroundImage:blueButtonImage forState:UIControlStateNormal];
     
+    
+    // Round corners of the optionsViewMat so it looks nicer
     _optionsViewMat.layer.cornerRadius = 5;
     _optionsViewMat.clipsToBounds = YES;
     
+    
+    // Create an info button and add it to the navigation bar
     UIButton *infoBtn = [UIButton buttonWithType:UIButtonTypeInfoLight];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:infoBtn];
     [infoBtn addTarget:self action:@selector(infoButtonPressed) forControlEvents:UIControlEventTouchUpInside];
@@ -90,46 +100,47 @@
 }
 
 - (void)viewWillDisappear:(BOOL)animated
-{
+{   // When the view disappears save the output results and the given options
     [super viewWillDisappear:YES];
     [td.outputArray replaceObjectAtIndex:QCGCDAndInverse withObject:[NSArray arrayWithObjects:[_gcd text], [_inverse text], nil]];
     [td.optionsList replaceObjectAtIndex:QCGCDAndInverse withObject:[NSArray arrayWithObjects:[_inverseOfField text], [_modField text], nil]];
 }
 
+
+#pragma mark - Rotation Methods
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-- (IBAction)dismissKeyboard:(id)sender
-{
-    [_inverseOfField resignFirstResponder];
-    [_modField resignFirstResponder];
-}
 
+#pragma mark - Caclulate Method
 - (IBAction)calculateButtonPressed:(id)sender
 {
     if( [[_inverseOfField text] isEqualToString:@""] || [[_modField text] isEqualToString:@""] )
-    {
+    {   // If the inverseOf and mod fields are emptpy, ask user to fill in
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Fill in Fields" message:@"Please fill in the Inverse of and Mod fields" delegate:nil cancelButtonTitle:@"Okay"otherButtonTitles:nil];
         [alert show];
     }
     else
-    {
+    {   // Get the results from method
         NSArray *results = [QCMethods GCDandInverse:[[_inverseOfField text] intValue] mod:[[_modField text] intValue]];
         if( [results count] == 2 )
-        {
+        {   // If there are results for both GCD and Inverse provide them
             _gcd.text = [[results objectAtIndex:0] stringValue];
             _inverse.text = [results objectAtIndex:1];
         }
         else
-        {
+        {   // If there are not results for both of them, an error has occured
+            /* NOTE: Error is due to Gary's method of getting GCD and Inverse in QCMethods, would like to fix in future update */
             _gcd.text = @"Error";
             _inverse.text = @"Error";
         }
     }
 }
 
+
+#pragma mark - Keyboard Methods
 - (void)addButtonToKeyboard {
 	// create custom button
 	UIButton *doneButton = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -173,14 +184,16 @@
     }
 }
 
-
-- (void)doneButton:(id)sender {
+- (IBAction)dismissKeyboard:(id)sender
+{
     [_inverseOfField resignFirstResponder];
     [_modField resignFirstResponder];
 }
 
+
+#pragma mark - Methods for Buttons in NavBar
 - (void)infoButtonPressed
-{
+{   // If the info button is pressed display the help blurb for GCDandInverse
     NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"HelpInfo" ofType:@"plist"];
     NSDictionary *helpDict = [NSDictionary dictionaryWithContentsOfFile:plistPath];
     NSArray *helpArray = [NSArray arrayWithArray:[helpDict valueForKey:@"QCHelpStrings"]];
