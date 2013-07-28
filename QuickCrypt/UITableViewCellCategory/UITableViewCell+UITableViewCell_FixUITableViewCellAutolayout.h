c46d6274f892be111da1742a8f320784468ce2c8
Joey Wessel <joey.wessel@gmail.com>
2013-07-28 04:25:42 -0400

    Added tableview cell category to fix autolayout issues on 6

---
+++ QuickCrypt/UITableViewCellCategory/UITableViewCell+UITableViewCell_FixUITableViewCellAutolayout.h
@@ -0,0 +1,13 @@
+//
+//  UITableViewCell+UITableViewCell_FixUITableViewCellAutolayout.h
+//  QuickCrypt
+//
+//  Created by Joey Wessel on 7/28/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableViewCell (UITableViewCell_FixUITableViewCellAutolayout)
+
+@end
---
+++ QuickCrypt/UITableViewCellCategory/UITableViewCell+UITableViewCell_FixUITableViewCellAutolayout.m
@@ -0,0 +1,13 @@
+//
+//  UITableViewCell+UITableViewCell_FixUITableViewCellAutolayout.m
+//  QuickCrypt
+//
+//  Created by Joey Wessel on 7/28/13.
+//
+//
+
+#import "UITableViewCell+UITableViewCell_FixUITableViewCellAutolayout.h"
+
+@implementation UITableViewCell (UITableViewCell_FixUITableViewCellAutolayout)
+
+@end
