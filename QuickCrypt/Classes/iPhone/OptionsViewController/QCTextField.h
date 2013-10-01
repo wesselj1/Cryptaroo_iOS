4be0b6965ca4052d617bd49d561abcf6df10013b
Joey Wessel <joey.wessel@gmail.com>
2013-10-01 04:36:49 -0400

    Added custom QCTextField

---
+++ QuickCrypt/Classes/iPhone/OptionsViewController/QCTextField.h
@@ -0,0 +1,13 @@
+//
+//  QCTextField.h
+//  QuickCrypt
+//
+//  Created by Joey Wessel on 8/22/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCTextField : UITextField
+
+@end
---
+++ QuickCrypt/Classes/iPhone/OptionsViewController/QCTextField.m
@@ -0,0 +1,32 @@
+//
+//  QCTextField.m
+//  QuickCrypt
+//
+//  Created by Joey Wessel on 8/22/13.
+//
+//
+
+#import "QCTextField.h"
+
+@implementation QCTextField
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        self.inputView.backgroundColor = [UIColor blackColor];
+    }
+    return self;
+}
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+    self.backgroundColor = [UIColor colorWithWhite:238/255.0 alpha:1.0];
+}
+
+@end
